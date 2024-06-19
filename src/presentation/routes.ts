1444d214e955cf18d809d002d03214979ed193d7
Gridman <santiagosuarez.beater@outlook.com>
2024-06-19 15:06:13 -0500

    updated Shell

---
+++ src/presentation/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+export class AppRoutes {
+  static get routes(): Router {
+    const router = Router();
+
+    // Definir las rutas
+    // router.use('/api/todos', /*TodoRoutes.routes */ );
+
+    return router;
+  }
+}
